f2144745d5693066114de1522729a6894db3194b
DaviAlencar2 <davilt.alencar@gmail.com>
2026-07-01 20:44:28 -0300

    feat: add setter to Area class and fix reject method in Article class

--- src/main/java/com/dah/domain/Area.java
+++ src/main/java/com/dah/domain/Area.java
@@ -2,9 +2,11 @@ package com.dah.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class Area {
   private Integer id;
   private String name;

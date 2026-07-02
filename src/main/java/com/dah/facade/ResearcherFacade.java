04d823a74821a3cc630d4df14abf3cc1cf5c2be8
Arthur <arthurfelipe5567@gmail.com>
2026-07-01 23:49:15 -0300

    feat: add remaining methods
    implements:
    - submitArticle
    - listMyArticles
    - listAssignedArticles
    - submitReview

--- src/main/java/com/dah/facade/ResearcherFacade.java
+++ src/main/java/com/dah/facade/ResearcherFacade.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.dah.domain.Area;
 import com.dah.domain.Article;
 import com.dah.domain.ReviewerProfile;
+import com.dah.exceptions.UnauthorizedOperationException;
 import com.dah.records.ArticleForReviewDTO;
 import com.dah.records.DefineReviewerAreasData;
 import com.dah.records.ReviewData;
 import com.dah.records.SubmitArticleData;
 import com.dah.service.AccountService;
+import com.dah.service.ArticleReviewService;
 import com.dah.service.CommitteeService;
 
 import lombok.AllArgsConstructor;
@@ -17,32 +19,34 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ResearcherFacade implements ResearcherActions {
 
-  // private final ArticleReviewService articleReviewService;
+  private final ArticleReviewService articleReviewService;
   private final CommitteeService committeeService;
   private final AccountService accountService;
 
   @Override
   public Article submitArticle(SubmitArticleData data) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'submitArticle'");
+    return articleReviewService.submitArticle(data);
   }
 
   @Override
   public List<Article> listMyArticles() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listMyArticles'");
+    return articleReviewService.listMyArticles();
   }
 
   @Override
   public List<ArticleForReviewDTO> listAssignedArticles() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listAssignedArticles'");
+    ReviewerProfile reviewer = committeeService.findReviewerByUser(accountService.getCurrentUser());
+
+    if (reviewer == null) {
+      throw new UnauthorizedOperationException("Usuário não é revisor e por isso não pode visualizar artigos assinalados a ele!");
+    }
+
+    return articleReviewService.listArticlesForReview(reviewer);
   }
 
   @Override
   public void submitReview(ReviewData data) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'submitReview'");
+    articleReviewService.submitReview(data);
   }
 
   @Override
